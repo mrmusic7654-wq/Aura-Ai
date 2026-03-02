9a5913851fbb8f00c509159bf4e2ed275d52ec8a
mrmusic7654-wq <mr.music7654@gmail.com>
2026-03-02 09:45:09 +0545

    Update MainActivity.kt

--- app/src/main/java/com/aura/ai/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/aura/ai/presentation/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.aura.ai.presentation.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
